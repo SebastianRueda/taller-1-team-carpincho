ad9ffcc9dbaebda88ab92a4dc6316b7c02f590a8
nicomarmiroli <49240185+nicomarmiroli@users.noreply.github.com>
2021-10-14 12:09:25 -0300

    denuncias de asistentes en progreso

---
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioDenuncia.java
@@ -0,0 +1,10 @@
+package ar.edu.unlam.tallerweb1.servicios;
+
+import java.util.List;
+
+import ar.edu.unlam.tallerweb1.modelo.Denuncia;
+
+public interface ServicioDenuncia {
+
+	List<Denuncia> traerDenuncia();
+}

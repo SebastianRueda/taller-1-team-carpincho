656f35d6c0bd47af2b0b8830632e13f686600894
E3MiLiAn0 <ovejero.emiliano@hotmail.es>
2021-11-20 11:44:10 -0300

    generarFactura repositorio test

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioFacturaImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioFacturaImpl.java
@@ -2,12 +2,31 @@ package ar.edu.unlam.tallerweb1.repositorios;
 
 import ar.edu.unlam.tallerweb1.modelo.Factura;
 import ar.edu.unlam.tallerweb1.modelo.Suscripcion;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository("repositorioFactura")
 public class RepositorioFacturaImpl implements RepositorioFactura{
+
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public RepositorioFacturaImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+
     @Override
     public void generarFactura(Factura factura) {
+        sessionFactory.getCurrentSession().save(factura);
 
+    }
+
+    @Override
+    public Factura buscarFacturaPorId(Long id) {
+        return (Factura) sessionFactory.getCurrentSession().createCriteria(Factura.class)
+                .add(Restrictions.eq("id", id)).uniqueResult();
     }
 }
